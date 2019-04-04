97843c143788197602e82310d110c572e68c9d43
Liango Liu <l.liu@gbh.com.do>
2019-04-04 08:17:25 -0400

    Add script that sets the APACHE_USER_ID to the user's id

---
+++ scripts/set-apache-uid.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This will give Apache the same permissions as the mounted folder
+# outside the container, for development purposes.
+
+echo "Assiging Apache UID..."
+sed -e "s|APACHE_USER_ID=.*|APACHE_USER_ID=$(id -u)|g" .env
+echo ""
